bccb75d852c592efc841b9b35e5400ad3b453b7f
Mark <m.evers@lukkien.com>
2024-12-12 19:31:15 +0100

    Change footer spacing

--- components/layout/Footer/Footer.jsx
+++ components/layout/Footer/Footer.jsx
@@ -4,19 +4,17 @@ import { Logo, FooterNavigation } from "components";
 
 const FooterDivider = () => {
   return (
-    <div className="h-0 w-3/4 border-t-[0.5px] border-[#edede3]/50 lg:hidden" />
+    <div className="h-0 w-3/4 border-t-[0.5px] border-[#edede3]/50 xl:hidden" />
   );
 };
 
 export const Footer = async ({}) => {
   return (
-    <footer className="flex w-full flex-col items-center justify-between gap-y-4 border-t-[0.5px] border-[#edede3]/50 bg-[#443b4a] px-6 py-4 lg:flex-row">
+    <footer className="flex w-full flex-col items-center justify-between gap-y-8 border-t-[0.5px] border-[#edede3]/50 bg-[#443b4a] px-6 py-4 2xl:flex-row">
       <Link href="/">
         <Logo size="medium" />
       </Link>
-      <FooterDivider />
       <FooterNavigation />
-      <FooterDivider />
       <div className="flex flex-col items-center justify-center gap-x-4 max-sm:gap-y-2 sm:flex-row">
         <p className="text-md text-center font-bold">
           &copy; 2024 Mark Evers. Licensed under the{" "}
