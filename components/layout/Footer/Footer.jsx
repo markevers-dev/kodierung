9141e9f7cbda099886fda4b4883d4e002ee60b31
Mark <m.evers@lukkien.com>
2024-12-12 19:01:39 +0100

    Change styling on Footer, Accordion and Support Page

--- components/layout/Footer/Footer.jsx
+++ components/layout/Footer/Footer.jsx
@@ -10,19 +10,29 @@ const FooterDivider = () => {
 
 export const Footer = async ({}) => {
   return (
-    <footer className="container flex w-full flex-col items-center justify-between space-y-4 border-t-[0.5px] border-[#edede3]/50 px-6 py-4 md:px-20 lg:flex-row">
+    <footer className="flex w-full flex-col items-center justify-between gap-y-4 border-t-[0.5px] border-[#edede3]/50 bg-[#443b4a] px-6 py-4 lg:flex-row">
       <Link href="/">
         <Logo size="medium" />
       </Link>
       <FooterDivider />
       <FooterNavigation />
       <FooterDivider />
-      <div className="flex flex-col items-center justify-center gap-x-4 max-sm:space-y-2 sm:flex-row">
-        <p className="text-md font-bold">&copy; 2024 GPL-3.0 license</p>
+      <div className="flex flex-col items-center justify-center gap-x-4 max-sm:gap-y-2 sm:flex-row">
+        <p className="text-md text-center font-bold">
+          &copy; 2024 Mark Evers. Licensed under the{" "}
+          <Link
+            href="https://github.com/markevers-dev/kodierung/blob/main/LICENSE"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-[#dfcaec] underline"
+          >
+            GNU General Public License v3.0
+          </Link>
+        </p>
         <Link
           href="https://github.com/markevers-dev/kodierung"
           target="_blank"
-          className="text-sm font-bold"
+          className="self-center text-sm font-bold"
         >
           <GitHubLogo />
         </Link>
